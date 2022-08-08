07e2e37438778d0a07f3a0ee539f3e8343b106b9
Adamcy-lvts <69142001+Adamcy-lvts@users.noreply.github.com>
2022-08-08 21:22:58 +0100

    Update user-dashboard.blade.php

--- resources/views/components/user-dashboard.blade.php
+++ resources/views/components/user-dashboard.blade.php
@@ -117,32 +117,130 @@
     </aside>
 
     <div class="w-full flex flex-col h-screen overflow-y-hidden">
-        <!-- Desktop Header -->
-        <header class="w-full items-center bg-white py-2 px-6 hidden sm:flex">
-            <div class="w-1/2"></div>
-            <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
-                <button @click="isOpen = !isOpen"
-                    class="realtive z-10 w-12 h-12 rounded-full overflow-hidden border-1 border-gray-400 hover:border-gray-300 focus:border-gray-300 focus:outline-none">
-                    <img src="{{ asset('storage/' . auth()->user()->profile_photo_path) }}">
-                </button>
-                <button x-show="isOpen" @click="isOpen = false"
-                    class="h-full w-full fixed inset-0 cursor-default"></button>
-                <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
-                    <a href="#" class="block px-4 py-2 account-link hover:text-gray-900">Account</a>
+        <!-- Dashboard Header -->
+        <div class="bg-green-600">
+            <div class="container mx-auto px-4">
+                <div class="flex items-center md:justify-between py-4">
+                    <div class="w-1/4 md:hidden">
+                        <svg class="fill-current text-white h-8 w-8" xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20">
+                            <path
+                                d="M16.4 9H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zm0 4H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1zM3.6 7h12.8c.552 0 .6-.447.6-1 0-.553-.048-1-.6-1H3.6c-.552 0-.6.447-.6 1 0 .553.048 1 .6 1z" />
+                        </svg>
+                    </div>
+                    <div class="w-1/2 md:w-auto text-center text-white text-2xl font-medium">
+                        Dashboard
+                    </div>
+                    <div class="w-1/4 md:w-auto md:flex text-right">
+                        <x-jet-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <div class="flex">
+                                    <button>
+                                        <img class="inline-block h-10 w-10 rounded-full"
+                                            src="{{ asset('storage/' . auth()->user()->profile_photo_path) }}"
+                                            alt="">
+                                    </button>
+                                    <div class="hidden md:block cursor-pointer md:flex md:items-center ml-2">
+                                        <span class="text-white text-sm mr-1">{{ auth()->user()->first_name }}</span>
+                                        <div>
+                                            <svg class="fill-current text-white h-4 w-4 block opacity-50"
+                                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                <path
+                                                    d="M4.516 7.548c.436-.446 1.043-.481 1.576 0L10 11.295l3.908-3.747c.533-.481 1.141-.446 1.574 0 .436.445.408 1.197 0 1.615-.406.418-4.695 4.502-4.695 4.502a1.095 1.095 0 0 1-1.576 0S4.924 9.581 4.516 9.163c-.409-.418-.436-1.17 0-1.615z" />
+                                            </svg>
+                                        </div>
+                                    </div>
+                                </div>
+                            </x-slot>
+                            <x-slot name="content">
+                                <!-- Account Management -->
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Manage Account') }}
+                                </div>
 
-                    <!-- Authentication -->
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
+                                <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('Profile') }}
+                                </x-jet-dropdown-link>
 
-                        <x-jet-dropdown-link href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
+                                <div class="border-t border-gray-100"></div>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-jet-dropdown-link href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
                                                 this.closest('form').submit();">
-                            {{ __('Log Out') }}
-                        </x-jet-dropdown-link>
-                    </form>
+                                        {{ __('Log Out') }}
+                                    </x-jet-dropdown-link>
+                                </form>
+                            </x-slot>
+
+                        </x-jet-dropdown>
+                    </div>
                 </div>
             </div>
-        </header>
+        </div>
+        <div class="hidden bg-blue-500 md:block md:bg-white md:border-b">
+            <div class="container mx-auto px-4">
+                <div class="md:flex">
+                    <div class="flex -mb-px mr-8">
+                        <a href="#"
+                            class="no-underline text-white md:text-blue-600 flex items-center py-4 border-b border-blue-500">
+                            <svg class="h-6 w-6 fill-gray-500 mr-2" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24">
+                                <path fill-rule="evenodd"
+                                    d="M3.889 3h6.222a.9.9 0 0 1 .889.91v8.18a.9.9 0 0 1-.889.91H3.89A.9.9 0 0 1 3 12.09V3.91A.9.9 0 0 1 3.889 3zM3.889 15h6.222c.491 0 .889.384.889.857v4.286c0 .473-.398.857-.889.857H3.89C3.398 21 3 20.616 3 20.143v-4.286c0-.473.398-.857.889-.857zM13.889 11h6.222a.9.9 0 0 1 .889.91v8.18a.9.9 0 0 1-.889.91H13.89a.9.9 0 0 1-.889-.91v-8.18a.9.9 0 0 1 .889-.91zM13.889 3h6.222c.491 0 .889.384.889.857v4.286c0 .473-.398.857-.889.857H13.89C13.398 9 13 8.616 13 8.143V3.857c0-.473.398-.857.889-.857z" />
+                            </svg> Dashboard
+                        </a>
+                    </div>
+                    <div class="flex -mb-px mr-8">
+                        <a href="#"
+                            class="no-underline text-white opacity-50 md:text-gray-500 md:opacity-100 flex items-center py-4 border-b border-transparent hover:opacity-100 md:hover:border-gray-500">
+                            <svg class="h-6 w-6 fill-gray-500 mr-2" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24">
+                                <path d="M8 7h10V5l4 3.5-4 3.5v-2H8V7zm-6 8.5L6 12v2h10v3H6v2l-4-3.5z"
+                                    fill-rule="nonzero" />
+                            </svg> Buy/Sell
+                        </a>
+                    </div>
+                    <div class="flex -mb-px mr-8">
+                        <a href="#"
+                            class="no-underline text-white opacity-50 md:text-gray-500 md:opacity-100 flex items-center py-4 border-b border-transparent hover:opacity-100 md:hover:border-gray-500">
+                            <svg class="h-6 w-6 fill-gray-500 mr-2" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24">
+                                <path
+                                    d="M18 8H5.5v-.5l11-.88v.88H18V6c0-1.1-.891-1.872-1.979-1.717L5.98 5.717C4.891 5.873 4 6.9 4 8v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8a2 2 0 0 0-2-2zm-1.5 7.006a1.5 1.5 0 1 1 .001-3.001 1.5 1.5 0 0 1-.001 3.001z"
+                                    fill-rule="nonzero" />
+                            </svg> Accounts
+                        </a>
+                    </div>
+                    <div class="flex -mb-px mr-8">
+                        <a href="#"
+                            class="no-underline text-white opacity-50 md:text-gray-500 md:opacity-100 flex items-center py-4 border-b border-transparent hover:opacity-100 md:hover:border-gray-500">
+                            <svg class="h-6 w-6 fill-gray-500 mr-2" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24">
+                                <path
+                                    d="M11 12h2v2h9s-.149-4.459-.2-5.854C21.75 6.82 21.275 6 19.8 6h-3.208l-1.197-2.256C15.064 3.121 14.951 3 14.216 3H9.783c-.735 0-.847.121-1.179.744-.165.311-.7 1.318-1.196 2.256H4.199c-1.476 0-1.945.82-2 2.146C2.145 9.473 2 14 2 14h9v-2zM9.649 4.916c.23-.432.308-.516.817-.516h3.067c.509 0 .588.084.816.516L14.924 6h-5.85l.575-1.084zM13 17h-2v-2H2.5s.124 1.797.199 3.322c.031.633.218 1.678 1.8 1.678H19.5c1.582 0 1.765-1.047 1.8-1.678.087-1.568.2-3.322.2-3.322H13v2z"
+                                    fill-rule="nonzero" />
+                            </svg> Tools
+                        </a>
+                    </div>
+                    <div class="flex -mb-px">
+                        <a href="#"
+                            class="no-underline text-white opacity-50 md:text-gray-500 md:opacity-100 flex items-center py-4 border-b border-transparent hover:opacity-100 md:hover:border-gray-500">
+                            <svg class="h-6 w-6 fill-gray-500 mr-2" xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24">
+                                <path
+                                    d="M18.783 12c0-1.049.646-1.875 1.617-2.443a8.932 8.932 0 0 0-.692-1.672c-1.089.285-1.97-.141-2.711-.883-.741-.74-.968-1.621-.683-2.711a8.732 8.732 0 0 0-1.672-.691c-.568.97-1.595 1.615-2.642 1.615-1.048 0-2.074-.645-2.643-1.615-.58.172-1.14.403-1.671.691.285 1.09.059 1.971-.684 2.711-.74.742-1.621 1.168-2.711.883A8.797 8.797 0 0 0 3.6 9.557c.97.568 1.615 1.394 1.615 2.443 0 1.047-.645 2.074-1.615 2.643.173.58.404 1.14.691 1.672 1.09-.285 1.971-.059 2.711.682.741.742.969 1.623.684 2.711.532.288 1.092.52 1.672.693.568-.973 1.595-1.617 2.643-1.617 1.047 0 2.074.645 2.643 1.617a8.963 8.963 0 0 0 1.672-.693c-.285-1.088-.059-1.969.683-2.711.741-.74 1.622-1.166 2.711-.883.287-.532.52-1.092.692-1.672-.973-.569-1.619-1.395-1.619-2.442zM12 15.652a3.653 3.653 0 1 1 0-7.306 3.653 3.653 0 0 1 0 7.306z"
+                                    fill-rule="nonzero" />
+                            </svg> Settings
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>{{-- End of dashboard header --}}
+
 
         <!-- Mobile Header & Nav -->
         <header x-data="{ isOpen: false }" class="w-full bg-gr bg-sidebar py-5 px-6 sm:hidden">
@@ -242,7 +340,7 @@
                 </header>
             @endif
             <!-- Page Content -->
-            <main class="w-full flex-grow p-6">
+            <main class=" bg-gray-100 w-full flex-grow p-6">
 
                 {{ $slot }}
             </main>
