64650d3c04a8a5791b216ad8b1af1abd250b9341
Adamcy-lvts <69142001+Adamcy-lvts@users.noreply.github.com>
2022-08-08 23:15:05 +0100

    Update show.blade.php

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -2,9 +2,9 @@
     {{-- <x-app-layout> --}}
     {{-- <div class="main-container"> --}}
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
+
+        {{ __('Profile') }}
+
     </x-slot>
 
     <div>
