6d9369603e7b060ecfe3f8bce9e98f966eea1f0a
Adamcy-lvts <69142001+Adamcy-lvts@users.noreply.github.com>
2022-08-11 15:26:19 +0100

    Update create-event.blade.php

--- resources/views/livewire/admin/event/create-event.blade.php
+++ resources/views/livewire/admin/event/create-event.blade.php
@@ -40,8 +40,9 @@
             <div class="flex items-center justify-center w-full">
                 <label class="flex flex-col w-full h-32 border-2 border-dashed hover:bg-gray-500 hover:border-gray-300">
                     <div class="flex flex-col items-center justify-center pt-7">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 text-gray-400 group-hover:text-gray-600"
-                            viewBox="0 0 20 20" fill="currentColor">
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            class="w-12 h-12 text-gray-400 group-hover:text-gray-600" viewBox="0 0 20 20"
+                            fill="currentColor">
                             <path fill-rule="evenodd"
                                 d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
                                 clip-rule="evenodd" />
@@ -80,7 +81,7 @@
             <x-slot name="footer">
                 <div class="flex items-center gap-x-3 justify-end">
                     <x-button wire:click="cancel" label="Cancel" flat />
-                    <x-button type="submit" id="submit" label="Create" spinner="save" primary />
+                    <x-button type="submit" id="submit" label="Create" spinner="save" />
                 </div>
             </x-slot>
 
