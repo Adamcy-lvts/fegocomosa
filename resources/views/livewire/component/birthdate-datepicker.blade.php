15b20ac7255318faac684c5c25d7523d5fe5896c
Adamcy-lvts <69142001+Adamcy-lvts@users.noreply.github.com>
2022-08-11 15:25:30 +0100

    Create birthdate-datepicker.blade.php

---
+++ resources/views/livewire/component/birthdate-datepicker.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <x-datetime-picker wire:model.defer="birthdate" without-time name="date_of_birth" :value="old('dateofbirth')"
+        label="Birthdate" placeholder="Birthdate" />
+</div>
