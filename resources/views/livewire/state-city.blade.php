03e8f2d667aef63a570b17ed304944ef793b5fc4
Adamcy-lvts <69142001+Adamcy-lvts@users.noreply.github.com>
2022-08-11 15:24:44 +0100

    Update state-city.blade.php

--- resources/views/livewire/state-city.blade.php
+++ resources/views/livewire/state-city.blade.php
@@ -1,39 +1,18 @@
 <div>
-     <div class="flex flex-wrap">
 
-                    <div class="w-full lg:w-6/12 px-2">
-                      <div class="relative w-full mb-3">
-                             <div class="mt-4">
-                                 <x-jet-label for="state" value="{{ __('State') }}" />
-                                 <select id="state-id"  wire:model="selectedState" name="state_id" class="border-0 px-3 py-3  rounded text-sm shadow focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-100  w-full " >
-                                       <option value="" disabled selected>--Select State--</option>
-                                        @foreach ($states as $state)
-                                            <option value="{{$state->id}}">{{$state->name}}</option>   
-                                       @endforeach
-                                 </select>
-                             </div>
-                       </div>
-                      </div>
+    <div class="col-span-1 sm:col-span-2 sm:grid sm:grid-cols-2 sm:gap-6">
+        <x-native-select class="sm:mb-0 mb-5" name="state_id" label="State" placeholder="State" wire:model="selectedState">
+            @foreach ($states as $state)
+                <option value="{{ $state->id }}">{{ $state->name }}</option>
+            @endforeach
+        </x-native-select>
 
-                      <div class="w-full lg:w-6/12 px-2">
-                        <div class="relative w-full mb-3">
-                             <div class="mt-4">
-                                 <x-jet-label for="city_id" value="{{ __('City') }}" />
-                                 <select id="lga" wire:model="selectedCity" name="city_id" class="border-0 px-3 py-3  rounded text-sm shadow focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-100  w-full "  >
-                                       <option value="" disabled selected>--Select L.G.A--</option>
-                                        @if (!is_null($cities))
-                                             
-                                            @foreach ($cities as $city)
-                                                <option value="{{ $city->id }}" >{{ $city->name }}</option>
-                                            @endforeach
-                                        @endif
-                                 </select>
-                             </div>
-                       </div>
-                      </div>
-
-                    
-
-
-             </div>
+        <x-native-select name="city_id" label="City" placeholder="City" wire:model="selectedCity">
+            @if (!is_null($cities))
+                @foreach ($cities as $city)
+                    <option value="{{ $city->id }}">{{ $city->name }}</option>
+                @endforeach
+            @endif
+        </x-native-select>
+    </div>
 </div>
