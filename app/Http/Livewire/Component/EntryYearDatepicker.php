178262c88c317949ff004e9e7d05a00142bd6605
Adamcy-lvts <69142001+Adamcy-lvts@users.noreply.github.com>
2022-08-11 15:27:53 +0100

    Create EntryYearDatepicker.php

---
+++ app/Http/Livewire/Component/EntryYearDatepicker.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Component;
+
+use Livewire\Component;
+
+class EntryYearDatepicker extends Component
+{
+    public function render()
+    {
+        return view('livewire.component.entry-year-datepicker');
+    }
+}
