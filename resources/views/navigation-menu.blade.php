8eddedda3d9a054ecff2939ff44275a52a87d9ce
Adamcy-lvts <69142001+Adamcy-lvts@users.noreply.github.com>
2022-09-20 16:33:51 +0100

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -45,6 +45,12 @@
                         {{ __('Projects') }}
                     </x-jet-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('category.index') }}" :active="request()->routeIs('category.index*')">
+                        {{ __('Members Category') }}
+                    </x-jet-nav-link>
+                </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('events') }}" :active="request()->routeIs('events*')">
                         {{ __('Events') }}
