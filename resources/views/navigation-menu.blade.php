b254890b801b632522cc08a52310b62f0bd0e800
Adamcy-lvts <lv4mj1@gmail.com>
2022-09-23 08:06:56 +0100

    navigation menu and campaign index update

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -237,7 +237,7 @@
                 @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
                     <div class="flex-shrink-0 mr-3">
                         <img class="h-10 w-10 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}"
-                            alt="{{ Auth::user()->name }}" />
+                            alt="{{ Auth::user()->username }}" />
                     </div>
                 @endif
 
