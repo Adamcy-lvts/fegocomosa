24d0a1a3f43268a9b21938a1cee45432f8224757
Adamcy-lvts <lv4mj1@gmail.com>
2022-09-23 07:56:59 +0100

    Update navigation-menu.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -37,7 +37,7 @@
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="{{ route('campaigns') }}" :active="request()->routeIs('campaigns*')">
-                        {{ __('Campaign') }}
+                        {{ __('Fundraisers') }}
                     </x-jet-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
@@ -214,7 +214,7 @@
             </x-jet-responsive-nav-link>
 
             <x-jet-responsive-nav-link href="{{ route('campaigns') }}" :active="request()->routeIs('campaigns*')">
-                {{ __('Campaign') }}
+                {{ __('Fundraisers') }}
             </x-jet-responsive-nav-link>
 
             <x-jet-responsive-nav-link href="{{ route('projects') }}" :active="request()->routeIs('projects*')">
