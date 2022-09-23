1fd13a15eebb0b5de31c7884b55c9db47ba734a8
Adamcy-lvts <lv4mj1@gmail.com>
2022-09-23 18:51:36 +0100

    Update welcome.blade.php

--- resources/views/livewire/welcome.blade.php
+++ resources/views/livewire/welcome.blade.php
@@ -53,24 +53,25 @@
             </div>
             <div class="animate__animated animate__slideInRight animate__slower flex flex-wrap -m-4">
                 @foreach ($events as $event)
-                    <div class="p-4 md:w-1/3">
-                        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                    <div class="p-4 md:w-1/3 ">
+                        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col shadow-lg">
                             <div class="flex items-center mb-3">
                                 <div
                                     class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-green-600 text-white flex-shrink-0">
                                     <i class="fal fa-calendar-check"></i>
                                 </div>
-                                <h2 class="text-gray-900 text-lg title-font font-medium">{{ $event->title }}</h2>
+                                <h2 class="text-gray-900 text-lg title-font uppercase font-medium">{{ $event->title }}
+                                </h2>
                             </div>
                             <div class="flex-grow">
                                 <div class="flex mb-4 justify-between">
-                                    <h1 class="text-6xl ">{{ presentDay($event->event_date) }}</h1>
-                                    <h3 class="text-5xl ">{{ presentMonth($event->event_date) }}</h3>
+                                    <h1 class="text-4xl md:text-6xl ">{{ presentDay($event->event_date) }}</h1>
+                                    <h3 class="text-3xl md:text-5xl ">{{ presentMonth($event->event_date) }}</h3>
                                 </div>
                                 <h3 class=""></h3>
 
-                                <small class="text-lg">Time: {{ timeFormat($event->event_time) }}</small>
-                                <p class="text-lg">Venue: {{ $event->event_venue }}
+                                <small class="text-sm md:text-lg">Time: {{ timeFormat($event->event_time) }}</small>
+                                <p class="text-sm md:text-lg">Venue: {{ $event->event_venue }}
                                 </p>
                                 <a class="mt-3 text-green-600 inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" stroke-linecap="round"
