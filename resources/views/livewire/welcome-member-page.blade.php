1bac7a6ef924230892c36ae03f470a2dd77e1691
Adamcy-lvts <lv4mj1@gmail.com>
2022-09-24 06:19:19 +0100

    Update welcome-member-page.blade.php

--- resources/views/livewire/welcome-member-page.blade.php
+++ resources/views/livewire/welcome-member-page.blade.php
@@ -27,13 +27,14 @@
             <div class="animate__animated animate__slideInRight animate__slower flex flex-wrap -m-4">
                 @foreach ($events as $event)
                     <div class="p-4 md:w-1/3">
-                        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+                        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col shadow-lg">
                             <div class="flex items-center mb-3">
                                 <div
                                     class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-green-600 text-white flex-shrink-0">
                                     <i class="fal fa-calendar-check"></i>
                                 </div>
-                                <h2 class="text-gray-900 text-sm md:text-lg title-font font-medium">{{ $event->title }}
+                                <h2 class="text-gray-900 text-sm md:text-lg title-font uppercase font-medium">
+                                    {{ $event->title }}
                                 </h2>
                             </div>
                             <div class="flex-grow">
