324ebae6cd661a905e3b88b2c498234a4d2ab94a
Adamcy-lvts <lv4mj1@gmail.com>
2022-09-25 09:41:13 +0100

    Update welcome-member-page.blade.php

--- resources/views/livewire/welcome-member-page.blade.php
+++ resources/views/livewire/welcome-member-page.blade.php
@@ -212,7 +212,7 @@
 
                         <div
                             class="h-full flex sm:flex-row flex-col items-center sm:justify-start justify-center text-center sm:text-left">
-                            <a href="{{ route('member.profile', $position->user->id) }}">
+                            <a href="{{ route('member.profile', $position->user->username) }}">
                                 <img alt="team"
                                     class="flex-shrink-0 rounded-lg w-48 h-48 object-cover object-center sm:mb-0 mb-4"
                                     src="{{ asset($position->user->profile_photo_url ?? '') }}">
@@ -220,7 +220,7 @@
                             <div class="flex-grow sm:pl-8">
 
                                 <h2 class="title-font font-medium text-lg text-gray-900"> <a
-                                        href="{{ route('member.profile', $position->user->id) }}">{{ ($position->user->first_name ?? '') . ' ' . ($position->user->last_name ?? '') }}
+                                        href="{{ route('member.profile', $position->user->username) }}">{{ ($position->user->first_name ?? '') . ' ' . ($position->user->last_name ?? '') }}
                                     </a>
                                 </h2>
                                 <h3 class="text-gray-500 mb-3">{{ $position->name }}</h3>
