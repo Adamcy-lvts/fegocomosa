20207e82a98837f709c4aa3b038c644810eca8ee
Adamcy-lvts <lv4mj1@gmail.com>
2022-09-30 22:33:45 +0100

    Update show-post.blade.php

--- resources/views/livewire/blog/show-post.blade.php
+++ resources/views/livewire/blog/show-post.blade.php
@@ -1,42 +1,50 @@
 <div>
+    @push('blog-styles')
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/content-styles.css') }}">
+    @endpush
 
     <section class="text-gray-600 body-font relative">
-        <div class="w-full  p-16  bg-gray-600 ">
+        <div class="w-full p-4  sm:p-16  bg-gray-600 ">
             <div class=" md:w-8/12 mx-auto">
-                <h1 class="antialiased lg:text-7xl text-3xl mb-5 font-semibold text-gray-200 font-mono">
+                <h1 class="antialiased sm:text-7xl text-lg mb-5 font-semibold text-gray-200 ">
                     {{ $post->title }}
                 </h1>
-                <h3 class="antialiased text-2xl text-gray-200 font-mono">True fans will remember these moments all too
+                {{-- <h3 class="antialiased text-base sm:text-2xl text-gray-200 ">True fans will remember these
+                    moments
+                    all too
                     well.
-                </h3>
+                </h3> --}}
             </div>
         </div>
         <div class="container px-5 py-24 pt-0 mx-auto flex flex-col">
 
             <div class="lg:w-5/6 mx-auto">
-                <div class="flex p-5 gap-3">
+                <div class="flex p-5 gap-3 overflow-x-auto">
                     <a class=" text-base text-gray-500 " href="{{ route('posts') }}">Blog</a>
                     <i class="fa-thin fa-chevron-right"></i>
                     <a class=" text-base text-gray-500 "
                         href="{{ route('category.post', $post->category->slug) }}">{{ $post->category->name }}</a>
                     <i class="fa-thin fa-chevron-right"></i>
-                    <a class=" text-base text-gray-500 font-bold">{{ $post->title }}</a>
+                    <a class="hidden md:block text-xs sm:text-base text-gray-500 font-bold">{{ $post->title }} </a>
                 </div>
                 <div class="flex flex-col sm:flex-row mt-10">
                     <div class="sm:w-1/3  text-center sm:pr-8 sm:py-8">
-                        <div
-                            class="w-32 h-32 rounded inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                            <img class="absolute object-cover w-32 h-32 rounded"
-                                src="{{ asset('storage/' . $post->user->profile_photo_path) }}" alt="Person" />
-                        </div>
-                        <div class="flex flex-col mb-5 items-center text-center justify-center">
+                        <div class="hidden md:block">
+                            <div
+                                class="w-32 h-32 rounded inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                                <img class="absolute object-cover w-32 h-32 rounded"
+                                    src="{{ asset('storage/' . $post->user->profile_photo_path) }}" alt="Person" />
+                            </div>
+                            <div class="flex flex-col mb-5 items-center text-center justify-center">
+                                <p class="mb-3 text-xs font-semibold text-gray-800">Author</p>
+                                <p class="text-lg font-bold">
+                                    {{ $post->user->first_name . ' ' . $post->user->last_name }}
+                                </p>
 
-                            <p class="text-lg font-bold">
-                                {{ $post->user->first_name . ' ' . $post->user->last_name }}
-                            </p>
-                            <p class="mb-3 text-xs text-gray-800">Author</p>
-                            <p class="text-sm">{{ $post->user->about_you }}</p>
+                                <p class="text-sm">{{ $post->user->about_you }}</p>
+                            </div>
                         </div>
+
 
 
                         <div class="hidden md:flex gap-4 flex-col">
@@ -71,13 +79,16 @@
                             @endforeach
                         </div>
                     </div>
+                    <h3 class="block sm:hidden text-sm font-bold text-gray-800"> Author:
+                        {{ $post->user->first_name . ' ' . $post->user->last_name }}
+                    </h3>
                     <div
-                        class="  sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                        <h3 class="text-sm font-bold">Posted : {{ $post->created_at->toFormattedDateString() }}
+                        class="  sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0  sm:text-left">
+                        <h3 class="text-sm font-bold">Posted on: {{ $post->created_at->toFormattedDateString() }}
                         </h3>
-                        <div class="w-full mb-5">
-                            <p class=" leading-relaxed text-lg mb-4">{!! $post->body !!}</p>
-                            <div class="mt-5 text-2xl">Show some love if you enjoyed it!
+                        <div class="ck-content mb-5">
+                            <p class=" leading-relaxed text-2xl mb-4">{!! $post->body !!}</p>
+                            <div class="mt-5 text-base">Show some love if you enjoyed it!
                                 <div class=" text-2xl mb-4">
                                     @if ($latestPost->likedBy(auth()->user()))
                                         <div class=" ">
