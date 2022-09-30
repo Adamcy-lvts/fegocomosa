da1c189016c74501853612966cd2631177928d7c
Adamcy-lvts <lv4mj1@gmail.com>
2022-09-30 11:17:42 +0100

    Update state-city field and phone filed

--- resources/views/livewire/state-city.blade.php
+++ resources/views/livewire/state-city.blade.php
@@ -3,7 +3,7 @@
     <div class="col-span-1 sm:col-span-2 sm:grid sm:grid-cols-2 sm:gap-6">
         <x-native-select
             class="border-gray-300 focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded-md sm:mb-0 mb-5"
-            name="state_id" label="State" placeholder="State" :value="old('state_id')" wire:model="selectedState">
+            name="state_id" label="State*" placeholder="State" :value="old('state_id')" wire:model="selectedState">
             @foreach ($states as $state)
                 <option value="{{ $state->id }}">{{ $state->name }}</option>
             @endforeach
@@ -11,7 +11,7 @@
 
         <x-native-select
             class="border-gray-300 focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 "
-            name="city_id" label="City" placeholder="City" wire:model="selectedCity" :value="old('city_id')">
+            name="city_id" label="City*" placeholder="City" wire:model="selectedCity" :value="old('city_id')">
             @if (!is_null($cities))
                 @foreach ($cities as $city)
                     <option value="{{ $city->id }}">{{ $city->name }}</option>
