610171c7d2c4e0ad6b64297d06c9ac2951168266
Adamcy-lvts <lv4mj1@gmail.com>
2022-09-30 05:39:02 +0100

    Update donation-form.blade.php

--- resources/views/donation-form/donation-form.blade.php
+++ resources/views/donation-form/donation-form.blade.php
@@ -10,47 +10,73 @@
                         green />
                 </div>
 
-                <div x-show="open" x-transition.duration.500ms class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <div x-show="open" x-transition.duration.500ms>
+                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-4">
 
-
-                    <div class="col-span-1 sm:col-span-2">
                         <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
                             label="First Name" placeholder="First Name" value="{{ auth()->user()->first_name }}"
                             name="fname" />
                         <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
                             label="Last Name" placeholder="Last Name" value="{{ auth()->user()->last_name }}"
                             name="lname" />
-                    </div>
 
-                    <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
-                        label="Email" placeholder="example@mail.com" name="email"
-                        value="{{ auth()->user()->email }}" />
 
-                    @livewire('component.phone-mask')
-
-                    <div class="col-span-1 sm:col-span-2">
                         <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
-                            label="Address" placeholder="Address" name="address"
-                            value="{{ auth()->user()->address }}" />
+                            label="Email" placeholder="example@mail.com" name="email"
+                            value="{{ auth()->user()->email }}" />
+
+                        @livewire('component.phone-mask')
                     </div>
 
-                    <div class="col-span-1 sm:col-span-2">
-                        @livewire('state-city')
-                    </div>
-                    {{-- @livewire('component.currency') --}}
-                    <div class="col-span-1 sm:col-span-2">
-                        <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
-                            label="Amount" placeholder="amount" name="amount" />
-                    </div>
+                    <div x-data="{ amount: '', open: true }">
 
-                    <input type="hidden" name="additional_info">
+                        <div class="flex flex-row gap-3 mb-4">
+
+                            <button type="button" x-on:click="amount = '500'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;500</span></button>
+
+
+                            <button type="button" x-on:click="amount = '1000'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;1000</span></button>
+
+
+                            <button type="button" x-on:click="amount = '5000'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;5,000</span></button>
+                        </div>
+                        <div class="flex flex-row gap-3 mb-4">
+                            <button type="button" x-on:click="amount = '10000'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;10,000</span></button>
+
+                            <button type="button" x-on:click="amount = '20000'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;20,000</span></button>
+
+                            <button type="button" x-on:click="amount = '50000'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;50,000</span></button>
+                        </div>
+
+                        <div class="col-span-1 sm:col-span-2 mb-4">
+                            <x-input type="hidden"
+                                class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+                                x-model.number='amount' name="amount" />
+
+                            <x-input x-show="open" type="number"
+                                class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+                                placeholder="Amount" name="amount" x-model.number='amount' />
+                        </div>
+                    </div>
 
                     <input type="hidden" name="metadata"
                         value="{{ json_encode($array = ['payment_for' => 'donation', 'member_id' => auth()->user()->id ?? '', 'campaign_id' => $campaign->id]) }}">
 
                     <input type="hidden" name="reference" value="{{ Paystack::genTranxRef() }}"> {{-- required --}}
 
-                    <div class="col-span-1 sm:col-span-2">
+                    <div class="col-span-1 sm:col-span-2 mb-4">
                         <x-textarea class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
                             label="Comment" placeholder="Comment" />
                     </div>
@@ -73,49 +99,95 @@
                         green />
                 </div>
 
-                <div x-show="open" x-transition.duration.500ms class="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <div x-show="open" x-transition.duration.500ms>
 
-                    <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
-                        label="First Name" placeholder="First Name" name="fname" />
-                    <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
-                        label="Last Name" placeholder="Last Name" name="lname" />
-
-                    <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
-                        label="Email" placeholder="example@mail.com" name="email" />
-
-                    @livewire('component.phone-mask')
-
-                    <div class="col-span-1 sm:col-span-2">
+                    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-3">
                         <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
-                            label="Address" placeholder="Address" name="address" />
-                    </div>
-
-                    <div class="col-span-1 sm:col-span-2">
-                        @livewire('state-city')
-                    </div>
-                    {{-- @livewire('component.currency') --}}
-                    <div class="col-span-1 sm:col-span-2">
+                            label="First Name" placeholder="First Name" name="fname" />
                         <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
-                            label="Amount" placeholder="amount" name="amount" />
+                            label="Last Name" placeholder="Last Name" name="lname" />
+
+                        <x-input class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+                            label="Email" placeholder="example@mail.com" name="email" />
+
+                        @livewire('component.phone-mask')
+
                     </div>
 
-                    <input type="hidden" name="additional_info">
 
+                    <div x-data="{ amount: '', open: true }">
+
+                        <div class="flex flex-row gap-3 mb-4">
+
+                            <button type="button" x-on:click="amount = '500'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;500</span></button>
+
+
+                            <button type="button" x-on:click="amount = '1000'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;1000</span></button>
+
+
+                            <button type="button" x-on:click="amount = '5000'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;5,000</span></button>
+                        </div>
+                        <div class="flex flex-row gap-3 mb-4">
+                            <button type="button" x-on:click="amount = '10000'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;10,000</span></button>
+
+                            <button type="button" x-on:click="amount = '20000'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;20,000</span></button>
+
+                            <button type="button" x-on:click="amount = '50000'*100; open = !open"
+                                class="p-4 flex-grow border focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50 rounded"><span>
+                                    &#x20A6;50,000</span></button>
+                        </div>
+
+                        <div class="col-span-1 sm:col-span-2 mb-4">
+                            <x-input type="hidden"
+                                class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+                                x-model.number='amount' name="amount" />
+
+                            <x-input x-show="open" type="number"
+                                class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+                                placeholder="Amount" name="amount" x-model.number='amount' />
+                        </div>
+                    </div>
                     <input type="hidden" name="metadata"
                         value="{{ json_encode($array = ['payment_for' => 'donation', 'campaign_id' => $campaign->id, 'member_id' => '']) }}">
 
                     <input type="hidden" name="reference" value="{{ Paystack::genTranxRef() }}">
                     {{-- required --}}
 
-                    <div class="col-span-1 sm:col-span-2">
-                        <x-textarea class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
+                    <div class="col-span-1 sm:col-span-2 mb-4">
+                        <x-textarea
+                            class="focus:border-green-300 focus:ring focus:ring-green-200 focus:ring-opacity-50"
                             label="Comment" placeholder="Comment" />
                     </div>
                     <div class="col-span-1 sm:col-span-2">
                         <x-button type="submit" label="Donate" class="w-full  py-2 px-8" green />
                     </div>
+
                 </div>
-            </div>
 
     </form>
 @endif
+
+@push('donation-script')
+    <script>
+        window.addEventListener('load', () => {
+
+            var inKobo = document.getElementById('amount').value;
+
+            var inNaira = inKobo * 100;
+
+            document.getElementById('amount').value = inNaira;
+
+            console.log(inNaira);
+        });
+    </script>
+@endpush
