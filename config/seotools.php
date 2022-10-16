c64aa311aa080c27cd6f365bdf40677d4224b03f
Adamcy-lvts <lv4mj1@gmail.com>
2022-10-16 14:18:08 +0100

    Update seotools.php

--- config/seotools.php
+++ config/seotools.php
@@ -58,7 +58,7 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'Over 9000 Thousand!', // set false to total remove
+            'title'       => false, // set false to total remove
             'description' => 'For those who helped create the Genki Dama', // set false to total remove
             'url'         => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
             'type'        => 'WebPage',
